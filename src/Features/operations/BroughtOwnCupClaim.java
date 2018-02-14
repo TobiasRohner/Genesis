80c257e4f9c26e9e4a3c02f4099a31f15f79e9ad
Tobias Rohner <rohner.tobias@gmail.com>
2018-02-14 17:22:56 +0100

    Add claim for bringing own cup to store

---
+++ src/Features/operations/BroughtOwnCupClaim.java
@@ -0,0 +1,15 @@
+package Features.operations;
+
+import Features.operations.actions.IAction;
+
+/**
+ * Created by Tobias on 14.02.2018.
+ */
+
+public class BroughtOwnCupClaim extends StandardClaim {
+
+    public BroughtOwnCupClaim(IAction action, IActionProof proof) {
+        super(action, proof);
+    }
+
+}
