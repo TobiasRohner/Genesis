74f6d553fac94ea4f1f97ae4967b20c52113b25d
Tobias Rohner <rohner.tobias@gmail.com>
2018-02-14 20:58:04 +0100

    Wrote some documentation on the claim verifying function

--- src/Features/operations/BringOwnCupOperation.java
+++ src/Features/operations/BringOwnCupOperation.java
@@ -49,16 +49,22 @@ public class BringOwnCupOperation extends AOperation {
         return false;
     }
 
-    /*
+    /**
+     * @brief Verifying a given claim with its proof
+     *
      * The proof for the claim should be a digital signature
      * on the hash of the action object.
      * The Signature object is updated with the least significant byte first
+     *
+     * @param claim The claim object to verify.
+     * @param public_key The public key of the instance that signed the data
+     * @param signature_algorithm The signature algorithm to be used
      */
     private boolean verifyClaim(IClaim claim,
-                             PublicKey public_key,
-                             String signatureAlgorithm) throws Exception {
+                                PublicKey public_key,
+                                String signature_algorithm) throws Exception {
         int hash = claim.getAction().hashCode();
-        Signature signature = Signature.getInstance(signatureAlgorithm);
+        Signature signature = Signature.getInstance(signature_algorithm);
         signature.initVerify(public_key);
         for (int i = 0 ; i < 4 ; ++i) {
             signature.update((byte)(hash >> 8*i));
